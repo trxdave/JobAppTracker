edb6d2cc9caa713dcd5d5f55f2858cc0603e0177
David Cotter <daveguy28@gmail.com>
2024-12-12 14:31:37 +0000

    final working website

--- job_application/models.py
+++ job_application/models.py
@@ -28,9 +28,6 @@ class Job(models.Model):
         ordering = ['-date_applied']
         verbose_name_plural = 'Jobs'
 
-class Goal(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    goal_type = models.CharField(max_length=20)  #
 
 class Goal(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
